139645ef5f2f3256a420e701288f33fd0cc37e4b
AndNik <an.pochta@gmail.com>
2022-03-16 00:57:40 +0300

    startJS

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+const personalMoveDb = {
+    count: numberOfFilms,
+    moves: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let incr = 10,
+//    decr = 10;
+
+//incr++; // увеличение на 1
+//decr--; // уменьшение на 1
+
+
+/* console.log('https://ya.ru/' + 'category' + '/'); // по старому  
+console.log(`https://ya.ru/$[{category}/`); // по новому так
+
+//const user = 'ivan'; //по старому 
+//alert(`Привет, ${user}`); //по новому так */
+
+// user привет 
+//let answers = [];
+//answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[2] = prompt('как  возраст?', '');
+// document.write(answers);
+
+// const persone = confirm('уверен');
+
+// console.log(persone);
