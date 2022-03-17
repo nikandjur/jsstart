36159c42714204e338fb58cfe2e3a71bb8e98e21
AndNik <an.pochta@gmail.com>
2022-03-17 22:28:14 +0300

    zad1begin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -8,9 +9,10 @@ const personalMoviesDB = {
 };
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');    
+        b = prompt('На сколько оцените его?', ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt('На сколько оцените его?', '');   
+
 personalMoviesDB.movies[a] = b;
 personalMoviesDB.movies[c] = d;
 console.log(personalMoviesDB);
